a346d16c6dd37564f5851ab7525ea82810e829d9
greene5952 <109919479+PassiveInuDev@users.noreply.github.com>
2022-07-25 15:16:25 -0500

    test minted

--- scripts/deployArgs.js
+++ scripts/deployArgs.js
@@ -1,6 +1,6 @@
 module.exports = [
-    'IGLPresale Presale',
-    'IGLPresalePNFT',
+    'IGL Presale NFT',
+    'IGLPNFT',
 
     '0x9C3f261e2cc4C88DfaC56A5B46cdbf767eE2f231',
     '0xc01aC67ba14F13Af4fCD5b4ba3784F39a703cda0',
--- test/Test-NFTPumpContract.js
+++ test/Test-NFTPumpContract.js
@@ -39,7 +39,7 @@ if (true == true)
             let ethBalance = ethers.utils.formatEther(await ethers.provider.getBalance(owner.address));
             console.log("Start Balance: " + ethBalance);
 
-            const currentContract = await ethers.getContractFactory("IGLPresalePresale");
+            const currentContract = await ethers.getContractFactory("IGLPresaleNFT");
             currentToken = await currentContract.deploy(
                 'Test Contract',
                 'Test',
