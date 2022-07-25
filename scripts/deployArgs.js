747712d3b67ac44479a81d27ea845fd67acd53f0
greene5952 <109919479+PassiveInuDev@users.noreply.github.com>
2022-07-25 14:50:25 -0500

    Update

--- scripts/deployArgs.js
+++ scripts/deployArgs.js
@@ -4,8 +4,8 @@ module.exports = [
 
     '0x9C3f261e2cc4C88DfaC56A5B46cdbf767eE2f231',
     '0xc01aC67ba14F13Af4fCD5b4ba3784F39a703cda0',
-    'https://tmc-suits.s3.us-west-1.amazonaws.com/IGLPresalePresale/json/',
-    'https://public-pre-ipfs.s3.amazonaws.com/IGLPresalePresale/assets/IGLPresaleReveal.json',
+    'https://public-pre-ipfs.s3.amazonaws.com/IGLPresale/json/',
+    'https://public-pre-ipfs.s3.amazonaws.com/IGLPresale/assets/IGLReveal.json',
 
     [
         ethers.utils.getAddress('0xD5d552F011F1c7A64837c06A689A4058cf6f8Bb9'),
