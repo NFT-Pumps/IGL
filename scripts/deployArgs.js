e27bca21b8b902f899ba2fe8c370f17e3b55f5d2
Techoshi <risi@eminent-it.com>
2022-08-05 16:31:22 -0400

    Update deployArgs.js

--- scripts/deployArgs.js
+++ scripts/deployArgs.js
@@ -1,30 +1,8 @@
 module.exports = [
     'IGL Presale NFT',
     'IGLPNFT',
-
     '0x9C3f261e2cc4C88DfaC56A5B46cdbf767eE2f231',
     '0xc01aC67ba14F13Af4fCD5b4ba3784F39a703cda0',
     'https://public-pre-ipfs.s3.amazonaws.com/IGLPresale/json/',
-    'https://public-pre-ipfs.s3.amazonaws.com/IGLPresale/assets/IGLReveal.json',
-
-    [
-        ethers.utils.getAddress('0xb731eCbea158A4B7a854acDeD84Eba9D03693953'),
-        ethers.utils.getAddress('OxaEda2774be030379C54da1541BCa04E77B0Be9B4'),
-        ethers.utils.getAddress('0xf886B127d4E381E7619d2Af1617476fef0d04F8c'),
-        ethers.utils.getAddress('0x763A1c99B8E3Bb0fAD1A229e160Ce9f10D36197E'),
-        ethers.utils.getAddress('0x54bC07d7F81B207427C3879F272597Bf02255B00'),
-        ethers.utils.getAddress('0x2638E15140EE7289edE2A04DE05198F013E6ACdd'),
-        ethers.utils.getAddress('0x4A4Eec7aB4010044e200A608f0F9CAE2AcfdEDE6'),
-        ethers.utils.getAddress('0x8912e40d291693C7A86C418D52b46766780C6989')
-    ],
-    [
-        71.5,
-        5,
-        2.5,
-        2.5,
-        2.5,
-        5,
-        1,
-        10
-    ]
+    'https://public-pre-ipfs.s3.amazonaws.com/IGLPresale/assets/IGLReveal.json'
 ]
